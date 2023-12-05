eaecd49f1c722bc1e89f1e28ff695098b247dbc0
Inotila <78792300+Inotila@users.noreply.github.com>
2023-12-05 13:27:02 +0100

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projecthangman {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projecthangman to javafx.fxml;
-    exports com.example.projecthangman;
-}
