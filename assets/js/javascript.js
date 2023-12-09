9409e1bfbc569f1486095e55b0138940a15e834a
Inotila <inoati1991@gmail.com>
2023-12-09 22:39:55 +0000

    Add a numbers of players variable and logic that extracts the numbers of players selected from the form in the play.html page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,26 +1,44 @@
 //Store player name, chosen word & _ _ _ dashed lines according to lenght of word in array
 
-let players = [];
+// let players = [];
 
 
 
 
-document.getElementById("player-info-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let name = document.getElementById("name").value;
-    let word = document.getElementById("word").value;
-    addPlayer(name, word);
+// document.getElementById("player-info-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     let name = document.getElementById("name").value;
+//     let word = document.getElementById("word").value;
+//     addPlayer(name, word);
+// });
+
+
+// function addPlayer(name, word){
+//     players.push({
+//         name: name,
+//         word: word,
+//         guess: "_ ".repeat(word.length)
+//     })
+//     console.log(players);
+// };
+
+
+// Default value of numbers of players
+let numberOfPlayers = 0; 
+
+// Access the form and select element
+const numberOfPlayersForm = document.getElementById('playerForm');
+const selectNumberOfPlayersElement = document.getElementById('playerCount');
+
+// Event listener for form submission
+numberOfPlayersForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevents the default form submission behavior
+
+  // Get the selected value from the dropdown
+  numberOfPlayers = parseInt(selectNumberOfPlayersElement.value);
+
+  // test log to see if it numberOfPlayers Updated
+  console.log(`Number of players selected: ${numberOfPlayers}`);
+
 });
-
-
-function addPlayer(name, word){
-    players.push({
-        name: name,
-        word: word,
-        guess: "_ ".repeat(word.length)
-    })
-    console.log(players);
-};
-
-
 
