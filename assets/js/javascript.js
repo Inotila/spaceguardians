78e0b2d2ccb9a149eaef8300f70058ee174839d0
Inotila <inoati1991@gmail.com>
2023-12-11 23:45:52 +0000

    Added player score to player object

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,13 +6,14 @@ let players = [];
 let addedPlayers = 0;
 //game play variables
 let testWord = "thewordw"; //sample word
+let secretWord = "";
 let matchingLetters = "";
 let incorrectLetters ="";
 let guessCounter = 0;
+let playerScore = 0;
 
-let displayWord = testWord.replaceAll(/\w/g, "_ ");
 
-document.getElementById("display_word").innerHTML = displayWord;
+
 // Access the form and select element
 const numberOfPlayersForm = document.getElementById('playerForm');
 const selectNumberOfPlayersElement = document.getElementById('playerCount');
@@ -49,7 +50,7 @@ numberOfPlayersForm.addEventListener('submit', function(event) {
   if (addedPlayers < numberOfPlayers){   
      let name = document.getElementById("name").value;
      let word = document.getElementById("word").value;
-     addPlayer(name, word);
+     addPlayer(name, word, playerScore);
      addedPlayers++;
 
      const playerInfoForm = document.getElementById("player-info-form");
@@ -74,14 +75,30 @@ numberOfPlayersForm.addEventListener('submit', function(event) {
 });
 
 
- function addPlayer(name, word){
+ function addPlayer(name, word,playerScore){
      players.push({
          name: name,
         word: word,
-         guess: "_ ".repeat(word.length)
+         guess: "_ ".repeat(word.length),
+         playerScore: playerScore
      })
     console.log(players);
+    addPlayerDetails();
 };
+
+// logic for game play
+
+function addPlayerDetails(){
+
+  for( i = 0; i < players.length; i++){
+    secretWord = players[i].word.replaceAll(/\w/g, "_ ");
+  }
+
+  console.log(`player${i} ; secret word now: ${secretWord} ; score ${playerScore}`)
+
+}
+
+document.getElementById("display_word").innerHTML = secretWord;
 
 function printDashedWord(){
   let divBox = document.getElementById("abc123"); // change id
@@ -99,11 +116,11 @@ function guess(){
 
     console.log(letter);
 
-    if(testWord.match(letter)){
+    if(secretWord.match(letter)){
         matchingLetters += letter;
         let checkIfMatch = new RegExp(`[^${matchingLetters}]`, 'g'); 
-        displayWord = testWord.replaceAll(checkIfMatch, "_ ");
-        document.getElementById("display_word").innerHTML = displayWord;
+        secretWord = secretWord.replaceAll(checkIfMatch, "_ ");
+        document.getElementById("display_word").innerHTML = secretWord
         console.log(true);
         console.log(matchingLetters);
     } else {
