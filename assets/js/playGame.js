b719799261b867b9728a0c3c8c965a68091538c0
Carl-JohanK kekkonen <Carljohank@hotmail.com>
2023-12-11 09:29:18 +0000

    geuss letters visual changes

--- assets/js/playGame.js
+++ assets/js/playGame.js
@@ -1,4 +1,5 @@
 let testWord = "thewordw"; //sampe word
+let list = "";
 
 let displayWord = testWord.replaceAll(/\w/g, "_ ");
 
@@ -6,12 +7,16 @@ document.getElementById("display_word").innerHTML = displayWord;
 
 function geuss(){
     let letter = document.getElementById("geuss").value;
+    list += letter;
+    let match = new RegExp(`[^${list}]`, 'g') 
     console.log(letter);
 
     if(testWord.match(letter)){
+        displayWord = testWord.replaceAll(match, "_ ");
+        document.getElementById("display_word").innerHTML = displayWord;
         console.log(true);
     } else {
-        console.log(false)
+        console.log(false);
     }
     document.getElementById("geuss").value = "";
 }
