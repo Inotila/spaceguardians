be332de25e0b6c1e5a5240b4a3edca060a5b9ca9
Inotila <inoati1991@gmail.com>
2023-12-11 08:55:34 +0000

    Added a form for the names and secret word of players

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,8 +3,6 @@
 // let players = [];
 
 
-
-
 // document.getElementById("player-info-form").addEventListener("submit", function(event) {
 //     event.preventDefault();
 //     let name = document.getElementById("name").value;
