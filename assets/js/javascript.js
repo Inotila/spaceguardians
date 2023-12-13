a5d1e62932c7bc679584f7ea2f66d50153aeec30
Philip Creutzer <yhmu23.creuph@folkuniversitetet.nu>
2023-12-13 12:32:29 +0000

    added javascript code, for choosing N of players

---
+++ assets/js/javascript.js
@@ -0,0 +1,36 @@
+// variables for choosing number of players
+let numberOfPlayers = 0;
+let currentPlayer = 1;
+let currentOpponent = -1;
+
+// add player variables
+let players = [];
+let addedPlayers = 0;
+
+// Access the form and select element
+const numberOfPlayersForm = document.getElementById('playerForm');
+const selectNumberOfPlayersElement = document.getElementById('playerCount');
+
+// Event listener for form submission
+numberOfPlayersForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevents the default form submission behavior
+
+  // Get the selected value from the dropdown
+  numberOfPlayers = parseInt(selectNumberOfPlayersElement.value);
+
+  // test log to see if it numberOfPlayers Updated
+  console.log(`Number of players selected: ${numberOfPlayers}`);
+
+
+  //   turn of the display of the form once the number of players selected has been submitted
+  if (numberOfPlayers > 0) {
+    document.getElementById("numbers-selected-row").style.display = "none";
+    document.getElementById("nameRow").style.display = "block";
+    // access the element of playerPromt 
+    // document.getElementById("playerPromt").innerText = `Player ${currentPlayer}`;
+
+    console.log("its working");
+  }
+  return numberOfPlayers
+
+});
